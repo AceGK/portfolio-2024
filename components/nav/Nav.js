4f5592e92a44b8a0730c97931d95ed9f0a6ee259
Ace <acekisch@gmail.com>
2024-01-15 21:30:17 -0800

    update nav

--- components/nav/Nav.js
+++ components/nav/Nav.js
@@ -2,6 +2,12 @@
 import styles from './Nav.module.scss';
 import { useState, useEffect } from 'react';
 
+import Envelope from '../../public/icons/envelope.svg';
+import House from '../../public/icons/house.svg';
+import User from '../../public/icons/user.svg';
+import Layers from '../../public/icons/layers.svg';
+import Kanban from '../../public/icons/kanban.svg';
+
 
 export default function Nav() {
 
@@ -38,19 +44,34 @@ export default function Nav() {
     <nav className={styles.nav}>
       <ul className={styles.primary}>
         <li>
-          <a href="#ace" className={`${activeSection === 'ace' ? styles.active : styles.inactive} code-heading`}>Home</a>
+          <a href="#ace" className={`${activeSection === 'ace' ? styles.active : styles.inactive} code-heading`}>
+            <House />
+            <span>home</span>
+          </a>
         </li>
         <li>
-          <a href="#skills" className={`${activeSection === 'skills' ? styles.active : styles.inactive} code-heading`}>skills</a>
+          <a href="#skills" className={`${activeSection === 'skills' ? styles.active : styles.inactive} code-heading`}>
+          <Layers />
+            <span>skills</span>
+          </a>
         </li>
         <li>
-          <a href="#projects" className={`${activeSection === 'projects' ? styles.active : styles.inactive} code-heading`}>projects</a>
+          <a href="#projects" className={`${activeSection === 'projects' ? styles.active : styles.inactive} code-heading`}>
+          <Kanban />
+            <span>projects</span>
+          </a>
         </li>
         <li>
-          <a href="#about" className={`${activeSection === 'about' ? styles.active : styles.inactive} code-heading`}>about</a>
+          <a href="#about" className={`${activeSection === 'about' ? styles.active : styles.inactive} code-heading`}>
+          <User />
+            <span>about</span>
+          </a>
         </li>
         <li>
-          <a href="#contact" className={`${activeSection === 'contact' ? styles.active : styles.inactive} code-heading`}>contact</a>
+          <a href="#contact" className={`${activeSection === 'contact' ? styles.active : styles.inactive} code-heading`}>
+            <Envelope />
+            <span>contact</span>
+          </a>
         </li>
       </ul>
     </nav>
