3624b5f8fbcfe60fa1ea93f82cc98d7fa7f96941
Ace <acekisch@gmail.com>
2024-01-16 10:01:03 -0800

    update stylesheets

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Inter, Rubik } from 'next/font/google'
 import '../app/styles/reset.scss'
 import '../app/styles/matrix.scss'
 import '../app/styles/stars.scss'
+import '../app/styles/swiper.scss'
+import '../app/styles/utility.scss'
 import '../app/styles/dividers.scss'
 import '../app/styles/globals.scss'
 
