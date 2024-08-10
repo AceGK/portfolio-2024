afefd603acc49faa7ee1d4bce81634eb8fbe7aa7
Ace <acekisch@gmail.com>
2024-08-09 19:24:03 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,21 +2,28 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  images : {
-    domains : ['dummyimage.com'],
+  images: {
+    domains: ['dummyimage.com'],
     minimumCacheTTL: 60,
   },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: [{loader: "@svgr/webpack", options:{icon: true }}]
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
     });
 
     return config;
   },
- 
-}
 
+  async redirects() {
+    return [
+      {
+        source: '/rico-pearson',
+        destination: 'https://photos.acekisch.com/-ricopearson/gallery',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-
-module.exports = nextConfig
+module.exports = nextConfig;
